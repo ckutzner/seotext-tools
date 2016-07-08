789328cd47f51354fca1d3b28fa7005277eeab14
ryuu <thursa@riesenheim.net>
2016-07-08 16:34:03 +0200

    begun work on main script seotext.py

---
+++ seotext.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import analysis
+import results
+
+# read in keywords, build dictionaries
+bodykw = {} # keywords in body text, pattern: Keyword, target count
+h1kw = {} # keywords in h1
+h2kw = {} # keywords in h2
+metakw = {} # keywords in meta description
+titlekw = {} # keywords in title tag
+
+
+# count words
+ 
