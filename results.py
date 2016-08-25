8cf13bfc0458fe2541b02a4b038c7e8679db5ea7
ryuu <thursa@riesenheim.net>
2016-08-25 15:50:35 +0200

    begun work on pretty-printing function
    added module test
    added static print statements and call to analysis.wordcount()

--- results.py
+++ results.py
@@ -1,3 +1,13 @@
 """ module for printing out results """
+import analysis
 
-print('Here is where you are:\n')
+def pretty(fn,wc_target,*args):
+    """function for pretty-printing results, fn is the filename"""
+    print("Here is where you are with {}:\n".format(fn)) # preamble
+    print(55*"="+"\n") # separator line + newline
+    print("Total word count:\t{}\t{}".format(analysis.wordcount(fn,wc_target)[0],analysis.wordcount(fn,wc_target)[1]))
+    print(50*"="+"\n") # separator line + newline
+
+
+if __name__ == "__main__": #test function
+    pretty("testdata/testtext.md",500)
