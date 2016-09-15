dcf44eda318b75351861e9bb2497255bca5a2407
Camilla Kutzner <develop@camilla-kutzner.de>
2016-09-15 14:41:46 +0200

    begun work on entering data via input file

--- enter_data.py
+++ enter_data.py
@@ -1,7 +1,20 @@
 """module for entering data into the jobs and keyword database"""
 
-# def readstuff(*args):
-    """quick-and-dirty function to read stuffin and return a dictionary of dictionaries, takes arbitrary number of dictionaries as arguments, takes arbitrary number of dictionaries as arguments"""
+# def readstuff(file, dictindex, *args):
+    """quick-and-dirty function to read stuff in, args: file, dictionary index, prepared dictionaries; requires module re"""
+    name = ''
+    tc = 300 # default value
+    with open(file, 'r') as data:
+        lines = data.readlines()
+    for l in lines:
+        if ': ' in l:
+            x = l.split(': ')
+            dictindex.append(x[0])
+        elif re.search('^\d{3,5}$',l):
+            tc = int(l)
+        else: 
+            name = l
+    return name, dictindex, tc
 
 if __name__ == "__main__":
     print("This module is not functional yet!")
