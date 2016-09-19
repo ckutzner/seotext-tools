28a1ee86fdf04d20033cf1d6a8e1dd9c530c5e76
ryuu <thursa@riesenheim.net>
2016-09-19 18:37:50 +0200

    work on function for reading in stuff from a file
    worked in first tests. requirements file still needs to be for one single text.

--- enter_data.py
+++ enter_data.py
@@ -1,17 +1,18 @@
 """module for entering data into the jobs and keyword database"""
 
-# def readstuff(file, dictindex, *args):
+import re
+
+def readstuff(dfile, dictindex, *args):
     """quick-and-dirty function to read stuff in, args: file, dictionary index, prepared dictionaries; requires module re"""
     name = ''
     tc = 300 # default value
-    with open(file, 'r') as data:
+    with open(dfile, 'r') as data:
         lines = data.readlines()
     for l in lines:
-        if ': ' in l:
-            x = l.split(': ')
-            dictindex.append(x[0])
-        elif re.search('^\d{3,5}$',l):
+        if re.match('\d{3,5}', l):
             tc = int(l)
+        elif ': ' in l:
+            dictindex.append(l.split(': ')[0])
         else: 
             name = l
     return name, dictindex, tc
